3b3445f9f075186d5b5222adc38d6a12183b9d54
Tobias Ww <twrt75@mni.thm.de>
2022-02-04 16:50:54 +0100

    rename

--- people_tsne.py
+++ people_tsne.py
@@ -1,12 +1,14 @@
 import numpy as np
 from sklearn.manifold import TSNE
-from sklearn.metrics import DistanceMetric
-from sklearn.metrics import pairwise_distances
 import matplotlib.pyplot as plt
 from sklearn.covariance import EmpiricalCovariance, MinCovDet
+from tueplots import bundles
+
+
+plt.rcParams.update(bundles.neurips2021())
 X = []
 import pickle
-with open("person_words.pickle","rb") as f:
+with open("persons_filtered_keywords.pickle","rb") as f:
     persons=pickle.load(f)
 for person in persons:
     X.append(person["word_count"])
@@ -22,6 +24,8 @@ colors = {"Q1387991":"yellow",
             "Q1826856":"purple",
            "Q2207512":"red"}
 
+
+
 parties = {"FDP": "Q1387991",
            "CDU": "Q1023134",
            "Grüne": "Q1007353",
@@ -30,8 +34,11 @@ parties = {"FDP": "Q1387991",
            "SPD": "Q2207512"}
 for i in range(len(X_embedded)):
     plt.plot(X_embedded[i][0],X_embedded[i][1],'ro',c=colors[persons[i]["factionID"]])
-plt.savefig("t-sne.png")
+
+plt.title("T-SNE of wc and politicians")
+plt.savefig("t-sne.pdf")
 plt.clf()
+exit(0)
 n_outliers=10
 robust_cov = MinCovDet().fit(X)
 # fit a MLE estimator to data
